93770e2f989491b903c0e0a0f0f0488c2fad073e
Migs2312 <101750039+Migs2312@users.noreply.github.com>
2024-10-21 14:55:10 -0300

    Final commit

--- eventif/wsgi.py
+++ eventif/wsgi.py
@@ -4,14 +4,15 @@ WSGI config for eventif project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+
 from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventif.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
