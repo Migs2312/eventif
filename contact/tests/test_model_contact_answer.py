5ae3de4439e52f45b795a719592caa997d21e57b
Miguel <miguel.martins@aluno.riogrande.ifrs.edu.br>
2024-12-16 16:28:06 -0300

    Toques finais no trabalho

--- contact/tests/test_model_contact_answer.py
+++ contact/tests/test_model_contact_answer.py
@@ -6,10 +6,10 @@ from django.core import mail
 class ContactModelTest(TestCase):
     def setUp(self):
         self.obj = Contact(
-            name='Cleber Fonseca',
-            email='profcleberfonseca@gmail.com',
+            name='Teste',
+            email='teste@gmail.com',
             phone='53-12345-6789',
-            message='Olá, estou entrando em contato',
+            message='Olá, estou testando o contato',
             response='Resposta do contato'
         )
         self.obj.save()
@@ -23,7 +23,7 @@ class ContactModelTest(TestCase):
         self.assertIsInstance(self.obj.sent_at, datetime)
 
     def test_str(self):
-        self.assertEqual('Cleber Fonseca', str(self.obj))
+        self.assertEqual('Teste', str(self.obj))
 
     def test_subscription_email_subject(self):
         expect = 'Resposta do seu contato'
@@ -34,15 +34,15 @@ class ContactModelTest(TestCase):
         self.assertEqual(expect, self.email.from_email)
 
     def test_subscription_email_to(self):
-        expect = ['contato@eventif.com.br', 'profcleberfonseca@gmail.com']
+        expect = ['contato@eventif.com.br', 'teste@gmail.com']
         self.assertEqual(expect, self.email.to)
 
     def test_subscription_email_body(self):
         contents = (
-            'Cleber Fonseca',
-            'profcleberfonseca@gmail.com',
+            'Teste',
+            'teste@gmail.com',
             '53-12345-6789',
-            'Olá, estou entrando em contato',
+            'Olá, estou testando o contato',
             'Resposta do contato'
         )
         for content in contents:
