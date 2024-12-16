d3f934d98aff569c8a4e5d66c7cf477fe1696d6d
Miguel <miguel.martins@aluno.riogrande.ifrs.edu.br>
2024-12-16 15:58:10 -0300

    Aparentemente não tinha um campo extra no contact

--- contact/admin.py
+++ contact/admin.py
@@ -4,7 +4,7 @@ from django import forms
 
 class ContactModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone', 'message',
-                    'sent_at', 'response', 'response_at')
+                    'sent_at', 'response', 'response_at', 'contact_answered')
     list_editable = ['response']
     date_hierarchy = 'sent_at'
     search_fields = ('name', 'email', 'phone', 'message', 'response', 'response_at', 'sent_at')
@@ -12,7 +12,7 @@ class ContactModelAdmin(admin.ModelAdmin):
     def contact_answered(self, obj):
         return obj.response != None
 
-    contact_answered.short_description = 'Contato respondido'
+    contact_answered.short_description = 'Resposta enviada'
     contact_answered.boolean = True
 
     def formfield_for_dbfield(self, db_field, request, **kwargs):
